251211d139b0d66a9eecb39921f0f92f68491938
Shataxi Dubey <shataxi.dubey@indiamart.com>
2022-08-01 11:31:24 +0530

    Revert "app"
    This reverts commit 95eea94b14978c7619257444043b743de6f78742.

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log('This is body section')
-document.getElementById("h1").innerHTML='<h1>This is H1 inside Div</h1>'
-document.getElementById("h1").style.color='white'
-document.getElementById("h1").style.backgroundColor='gray'
-
-function CallMe(){
-    alert('Clicked the button???')
-}
